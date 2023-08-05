632a301cf4d549f93abf5ca120174fd3e4618fb8
Nathaniel Hagan <hagannat@msu.edu>
2023-08-05 08:52:13 -0400

    Added basic tictactoe functionality. Todo: unit testing for tictactoe

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TicTacToe;
+
+TicTacToeBoard board = new TicTacToeBoard();
+
+Console.WriteLine(board);
+Console.WriteLine();
+board.MakeMove(1);
+board.MakeMove(2);
+board.MakeMove(5);
+board.MakeMove(3);
+board.MakeMove(9);
+
+Player winner;
+board.getWinner(out winner);
+Console.WriteLine(winner);
---
+++ TicTacToe/TicTacToeBoard.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class TicTacToeBoard
+    {
+        public Player Turn { get; private set; }
+        public Player[,] Board { get; private set; }
+
+        public TicTacToeBoard()
+        {
+            Board = new Player[3, 3];
+            Turn = Player.PlayerOne;
+        }
+
+        public TicTacToeBoard(Player Turn, Player[,] Board)
+        {
+            this.Turn = Turn;
+            this.Board = Board;
+        }
+
+        private void SwitchTurn()
+        {
+            if( Turn == Player.PlayerOne ) 
+            {
+                Turn = Player.PlayerTwo;
+            }
+            else
+            {
+                Turn = Player.PlayerOne;
+            }
+        }
+
+        public bool MakeMove(int square)
+        {
+            square--;
+            int row = square / 3;
+            int col = square % 3;
+
+            return MakeMove(row, col);
+        }
+
+        public bool MakeMove(int row, int col)
+        {
+            if (Board[row, col] != Player.None)
+            {
+                return false;
+            }
+
+            Board[row, col] = Turn;
+            SwitchTurn();
+            return true;
+        }
+
+        public bool getWinner(out Player winner)
+        {
+            winner = Player.None;
+
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                // Check rows
+                if (Board[i, 0] == Board[i, 1] && Board[i, 0] == Board[i, 2] && Board[i, 0] != Player.None)
+                {
+                    winner = Board[i, 0];
+                    return true;
+                }
+
+                // Check Columns
+                if (Board[0, i] == Board[1, i] && Board[0, i] == Board[2, i] && Board[0, i] != Player.None)
+                {
+                    winner = Board[0, i];
+                    return true;
+                }
+            }
+
+            // Check diagonals
+            if (Board[0, 0] == Board[1, 1] && Board[0, 0] == Board[2, 2] && Board[0, 0] != Player.None)
+            {
+                winner = Board[0, 0];
+                return true;
+            }
+
+            if (Board[0, 2] == Board[1, 1] && Board[0, 2] == Board[2, 0] && Board[0, 2] != Player.None)
+            {
+                winner = Board[0, 2];
+                return true;
+            }
+
+            // Check if draw
+            bool isDraw = true;
+            for(int i = 0; i <Board.GetLength(0); i++)
+            {
+                for(int j = 0; j < Board.GetLength(1); j++)
+                {
+                    if (Board[i,j] != Player.None)
+                    {
+                        isDraw = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isDraw)
+            {
+                winner = Player.None;
+                return true;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string output_string = "";
+
+            for (int i = 0; i < Board.GetLength(0); i++)
+            {
+                for (int j = 0; j < Board.GetLength(1); j++)
+                {
+                    var playerString = Board[i, j] switch
+                    {
+                        Player.PlayerOne => " X ",
+                        Player.PlayerTwo => " O ",
+                        _ => "   "
+                    };
+                    output_string += playerString;
+                    if (j < Board.GetLength(1) - 1)
+                    {
+                        output_string += "|";
+                    }
+                }
+                if (i < Board.GetLength(0) - 1)
+                {
+                    output_string += '\n';
+                    output_string += new string('-', Board.GetLength(1) * 4 - 1) + "\n";
+                }
+            }
+
+            return output_string;
+        }
+    }
+
+    public enum Player
+    {
+        None,
+        PlayerOne,
+        PlayerTwo
+    }
+
+}
