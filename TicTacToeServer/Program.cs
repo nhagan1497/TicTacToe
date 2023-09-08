1a0c7f06584c9106f0e1277a2abcec59089d0f87
Nathaniel Hagan <hagannat@msu.edu>
2023-09-08 18:57:19 -0400

    Added code for manually pushing actions to the server via command prompt while the UI is running to help with testing

--- TicTacToeServer/Program.cs
+++ TicTacToeServer/Program.cs
@@ -1,19 +1,61 @@
 ﻿using System.Text.Json;
 using TicTacToeNetwork;
 
+// Instance of the server to handle requests for the UI that's being tested
 using TicTacToeServer server = new();
-using TicTacToeClient player1 = new();
+
+// Instance of the client so I can push commands more conveniently from the command line to test functionality from the UI
 using TicTacToeClient player2 = new();
 
+// Starts the server and keeps the task for awaiting the shutdown
+Task runningServer = server.Start();
 
-Task serverRun = server.Start();
+Console.WriteLine("Server is running. Type commands for the client, help for options, and exit to quit. Case insensitive.");
 
-await player1.StartNewGame();
-await player2.StartNewGame();
 
-await player1.MakeMove(1);
-await player2.MakeMove(2);
-await player2.MakeMove(2);
+string? command = " ";
+bool response = true;
+
+while (command != "exit")
+{
+    command = Console.ReadLine();
+    if (String.IsNullOrEmpty(command))
+        continue;
+    command = command.ToLower();
+    switch(command)
+    {
+        case "newgame":
+            response = await player2.StartNewGame();
+            Console.WriteLine(response);
+            break;
+
+        case "move":
+            Console.WriteLine("Enter what number square:");
+
+            string? squareString = Console.ReadLine();
+            if (String.IsNullOrEmpty(squareString))
+                break;
+            int square = Int32.Parse(squareString);
+            response = await player2.MakeMove(square);
+            Console.WriteLine(response);
+            break;
+        case "getboard":
+            response =await player2.GetBoard();
+            Console.WriteLine(response);
+            break;
+        case "help":
+            Console.WriteLine("Commands:");
+            Console.WriteLine("    NewGame");
+            Console.WriteLine("    Move");
+            Console.WriteLine("    GetBoard");
+            Console.WriteLine("    Exit");
+            Console.WriteLine("    Help");
+            break;
+        default:
+            break;
+    }
+
+}
 
 server.Stop();
-await serverRun;
+await runningServer;
